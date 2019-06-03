b8eff72c687fb80e9304279235cc55746d720d81
Taen Ahammed <taenahammed7@gmail.com>
2019-06-03 15:24:06 +0600

    inheritance

---
+++ 08_inheritance.php
@@ -0,0 +1,43 @@
+<?php
+
+// Parent class / Super class
+class User
+{
+    protected $userName;
+    protected $userId;
+
+    public function __construct($userName, $userId)
+    {
+        $this->userName = $userName;
+        $this->userId = $userId;
+    }
+
+    public function userInfo()
+    {
+        echo ("UserName is: {$this->userName} and UserId is: {$this->userId} <br/>");
+    }
+}
+
+// Child class / sub class
+class Admin extends User
+{
+    public $level;
+
+    public function userInfo()
+    {
+        echo ("UserName is: {$this->userName} and UserId is: {$this->userId} and level is: {$this->level} <br/>");
+    }
+}
+
+$userName = "Taen Ahammed";
+$userId = 10;
+
+$user = new User($userName, $userId);
+$user->userInfo();
+
+
+$level = "Administrator";
+
+$admin = new Admin("Admin", 1);
+$admin->level = $level;
+$admin->userInfo();
